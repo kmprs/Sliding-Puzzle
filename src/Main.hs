72ef1d9766aeefaa240cf6036f70b583b089ff09
bkk <118755879+bkkarsten@users.noreply.github.com>
2025-05-10 13:43:40 +0200

    feat: include A* and 4x4 sliding puzzles in main demonstration

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,5 @@
 module Main where
-import PuzzleLogic.Helpers (isSolvable, printPath)
+import PuzzleLogic.Helpers (isSolvable, printPath, printState)
 import PuzzleLogic.PuzzleLogic (nextStates2D)
 import PuzzleLogic.Heuristic (manhattan)
 import Search.BFS (bfs) 
@@ -8,36 +8,47 @@ import Search.AStar (astar)
 
 main :: IO ()
 main = do 
-    let start = [[6, 7, 8],
-                 [4, 1, 2],
-                 [3, 5, 0]]
+    let start3 = [[6, 7, 8],
+                  [4, 1, 2],
+                  [3, 5, 0]]
 
-    -- let start = [[1, 2, 3],
-    --               [4, 5, 6],
-    --               [7, 0, 8]]
+    let target3 = [[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, 0]]
 
-    let target = [[1, 2, 3],
-                  [4, 5, 6],
-                  [7, 8, 0]]
+    let start4 = [[5,  1,  7,  3],
+                  [9,  2, 11,  4],
+                  [13, 6, 15,  8],
+                  [0, 10, 14, 12]]
 
---    let start = [[1,  2,  3,  4],
---                 [5,  6,  7,  8],
---                 [9, 10, 11, 12],
---                 [13, 15, 14, 0]]
---
---    let target = [[1,  2,  3,  4],
---                  [5,  6,  7,  8],
---                  [9, 10, 11, 12],
---                  [13, 14, 15, 0]]
+    let target4 = [[1,  2,  3,  4],
+                   [5,  6,  7,  8],
+                   [9, 10, 11, 12],
+                   [13,14, 15,  0]]
 
-    if isSolvable start target 
+    putStrLn "Solving a 3x3 sliding puzzle with starting configuration:"
+    printState start3
+    if isSolvable start3 target3
         then do
             putStrLn "Using BFS:"
-            case bfs start target nextStates2D of
+            case bfs start3 target3 nextStates2D of
                 Nothing -> putStrLn "Couldn't find a path to the target."
                 Just path -> printPath path
             putStrLn "Using A*:"
-            case astar start target nextStates2D manhattan of
+            case astar start3 target3 nextStates2D manhattan of
+                Nothing -> putStrLn "Couldn't find a path to the target."
+                Just path -> printPath path
+        else putStrLn "Configuration is not solvable."
+    putStrLn "Solving a 4x4 sliding puzzle with starting configuration:"
+    printState start4
+    if isSolvable start4 target4
+        then do
+            putStrLn "Using BFS:"
+            case bfs start4 target4 nextStates2D of
+                Nothing -> putStrLn "Couldn't find a path to the target."
+                Just path -> printPath path
+            putStrLn "Using A*:"
+            case astar start4 target4 nextStates2D manhattan of
                 Nothing -> putStrLn "Couldn't find a path to the target."
                 Just path -> printPath path
         else putStrLn "Configuration is not solvable."
